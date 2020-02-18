4f944cc81276943a9d2fd61b756ce6eb1c783970
stupns <stupns@i.ua>
2020-02-18 11:14:08 +0200

    add new file not_yelling.py for homework 13.02.20

---
+++ not_yelling.py
@@ -0,0 +1,14 @@
+# Write a function taking in a string like WOW this is REALLY amazing and returning Wow this is really amazing.
+# String should be capitalized and properly spaced. Using re and string is not allowed.
+
+
+def filter_words(st):
+    a = st.lower()
+    b = a.upper()[0] + a[1:]
+    c = b.split()
+    return " ".join(c)
+
+
+if __name__ == '__main__':
+    assert filter_words('HELLO world!') == 'Hello world!'
+    print("Well job!")
